9b6b8293d11b7e25bf8b3b28e87a3166e3892531
ThStartBoss <101006760+ThStartBoss@users.noreply.github.com>
2022-03-17 12:20:42 -0300

    Create questão1.js

---
+++ questão1.js
@@ -0,0 +1,58 @@
+    let aluno1 = "Antonio Alba"
+    let aluno2 = "Tiago Martins"
+    let aluno3 = "Igor Blaska"
+    let aluno4 = "Livia Sebag"
+    let aluno5 = "Emilio Freitas"
+    let aluno6 = "Ian Amaral"
+
+    console.log(aluno1)
+    console.log(aluno2)
+    console.log(aluno3)
+    console.log(aluno4)
+    console.log(aluno5)
+    console.log(aluno6)
+
+IsJogado = true
+console.log(IsJogado)
+IsFudido = true
+console.log(IsFudido)
+IsRodado = false
+console.log(IsRodado)
+IsViado = false
+console.log(IsViado)
+IsPadrao = true
+console.log(IsPadrao)
+
+let n1 = 56
+console.log (n1)
+let n2 = 87
+console.log (n2)
+let n3 = 11
+console.log (n3)
+let n4 = 15
+console.log(n4)
+let n5 = 9
+console.log(n5)
+
+var s1 = 900
+console.log (s1)
+var s2 = 11200
+console.log (s2)
+var s3 = 6900
+console.log (s3)
+var s4 = 3789
+console.log (s4)
+var s5 = 2000
+console.log (s5)
+
+
+const SALVE = 697
+console.log(SALVE);
+const KAO = 2
+console.log(KAO);
+const AMOR = 69
+console.log(AMOR);
+const ROUBO = 157
+console.log(ROUBO);
+const FURTO = 171
+console.log(FURTO);
